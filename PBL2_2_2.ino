598b2546eab22cc353e6e387e4f75ff2732db28f
sk-so <76169386+sk-so@users.noreply.github.com>
2021-02-05 13:21:13 +0900

    Update PBL2_2_2.ino

--- PBL2_2_2.ino
+++ PBL2_2_2.ino
@@ -1,4 +1,4 @@
-//進むときは三足
+//進むときは三足 42.89s 13.78s 12.30s 11.85s 12.44s 12.05s
 #include <Servo.h>
 
 Servo myservo_a_1; //左前脚の第一関節
@@ -9,6 +9,8 @@ Servo myservo_a_2; //左前脚の第二関節
 Servo myservo_b_2; //右前脚の第二関節
 Servo myservo_c_2; //左後脚の第二関節
 Servo myservo_d_2; //右後脚の第二関節
+
+int s = 140;
 
 void setup() {
   // put your setup code here, to run once:
@@ -21,56 +23,68 @@ void setup() {
   myservo_c_2.attach(12);
   myservo_d_2.attach(13);
   
-  myservo_a_2.write(90);
-  myservo_b_2.write(90);
-  myservo_c_2.write(90);
-  myservo_d_2.write(90);
+  myservo_a_2.write(70);//70
+  myservo_b_2.write(115);//110
+  myservo_c_2.write(100);//110
+  myservo_d_2.write(85);//70
   delay(500);
 
-  myservo_a_1.write(40);
-  myservo_b_1.write(80);
-  myservo_c_1.write(140);
-  myservo_d_1.write(100);
+  myservo_a_1.write(140);
+  myservo_b_1.write(100);
+  myservo_c_1.write(40);
+  myservo_d_1.write(80);//80
   delay(500);
 
 }
 
 void zenshin(){
-  myservo_b_2.write(180);
-  delay(500);
-  myservo_b_1.write(170);
-  delay(500);
-  myservo_b_2.write(90);
-  delay(500);
+  /*
+  myservo_b_1.write(90);
+  delay(200);
+  */
+  myservo_b_2.write(180); //180
+  delay(s);
+  myservo_b_1.write(10);
+  delay(s);
+  myservo_b_2.write(115);//110
+  delay(s);
   
-  myservo_a_1.write(100);
-  myservo_b_1.write(140);
+  myservo_a_1.write(80); //80
+  myservo_b_1.write(40);
+  //myservo_c_2.write(180);
+  myservo_d_1.write(140); //140
+  delay(s);
+
   myservo_c_2.write(180);
-  myservo_d_1.write(40);
-  delay(500);
+  delay(s);
+  myservo_c_1.write(100);
+  delay(s);
+  myservo_c_2.write(100);//110
+  delay(s);
 
-  myservo_c_1.write(80);
-  delay(500);
-  myservo_c_2.write(90);
-  delay(500);
+  /*
+  myservo_a_1.write(90);
+  delay(200);
+  */
+  myservo_a_2.write(0); //0
+  delay(s);
+  myservo_a_1.write(170);
+  delay(s);
+  myservo_a_2.write(70);//70
+  delay(s);
 
-  myservo_a_2.write(0);
-  delay(500);
-  myservo_a_1.write(10);
-  delay(500);
-  myservo_a_2.write(90);
-  delay(500);
+  myservo_a_1.write(140);
+  myservo_b_1.write(100);
+  myservo_c_1.write(40);
+  //myservo_d_2.write(0);
+  delay(s);
 
-  myservo_a_1.write(40);
-  myservo_b_1.write(80);
-  myservo_c_1.write(140);
   myservo_d_2.write(0);
-  delay(500);
-
-  myservo_d_1.write(100);
-  delay(500);
-  myservo_d_2.write(90);
-  delay(500);
+  delay(s);
+  myservo_d_1.write(80);//80
+  delay(s);
+  myservo_d_2.write(85);//70
+  delay(s);
 }
 
 void loop() {
